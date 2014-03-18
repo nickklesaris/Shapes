2d56b072dab694ab47b641aa70b229a842fb5f1b
Eric Kelly <heroiceric@gmail.com>
2014-03-18 13:59:38 -0400

    Refactor Rectangle specs
    It’s often helpful to group your tests inside of a `describe` block for
    each method the method that they’re testing.
    
    Since most of the tests are not testing any different behavior, just
    mode examples of the same behavior, I think this is one of the cases
    where it’s ok to have more than one expectation in the same `it` block.
    This just helps with the readability of the file

--- spec/models/rectangle_spec.rb
+++ spec/models/rectangle_spec.rb
@@ -2,43 +2,21 @@ require 'rspec'
 require_relative '../../models/rectangle'
 
 describe Rectangle do
-  it 'calculates the area' do
-    shape = Rectangle.new(10.0, 5.0)
-    expect(shape.area).to eq(50.0)
+  describe "#area" do
+    it 'calculates the area' do
+      expect(Rectangle.new(10.0, 5.0).area).to eq(50.0)
+      expect(Rectangle.new(17.0, 31.0).area).to eq(527.0)
+      expect(Rectangle.new(10.0, 2.0).area).to eq(20.0)
+      expect(Rectangle.new(111.0, 92.0).area).to eq(10212.0)
+    end
   end
 
-  it 'calculates the area' do
-    shape = Rectangle.new(17.0, 31.0)
-    expect(shape.area).to eq(527.0)
-  end
-
-  it 'calculates the area' do
-    shape = Rectangle.new(10.0, 2.0)
-    expect(shape.area).to eq(20.0)
-  end
-
-  it 'calculates the area' do
-    shape = Rectangle.new(111.0, 92.0)
-    expect(shape.area).to eq(10212.0)
-  end
-
-  it 'calculates the perimeter' do
-    shape = Rectangle.new(10.0, 5.0)
-    expect(shape.perimeter).to eq(30.0)
-  end
-
-  it 'calculates the perimeter' do
-    shape = Rectangle.new(17.0, 31.0)
-    expect(shape.perimeter).to eq(96.0)
-  end
-
-  it 'calculates the perimeter' do
-    shape = Rectangle.new(10.0, 2.0)
-    expect(shape.perimeter).to eq(24.0)
-  end
-
-  it 'calculates the perimeter' do
-    shape = Rectangle.new(111.0, 92.0)
-    expect(shape.perimeter).to eq(406.0)
+  describe "#perimeter" do
+    it 'calculates the perimeter' do
+      expect(Rectangle.new(10.0, 5.0).perimeter).to eq(30.0)
+      expect(Rectangle.new(17.0, 31.0).perimeter).to eq(96.0)
+      expect(Rectangle.new(10.0, 2.0).perimeter).to eq(24.0)
+      expect(Rectangle.new(111.0, 92.0).perimeter).to eq(406.0)
+    end
   end
 end
